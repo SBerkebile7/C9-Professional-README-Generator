e51945c2e1e0c0bfe676fd8dc90299219831402c
Spenny Berks <sberkebile@live.com>
2021-06-21 20:40:50 -0400

    final touches, license badge showing properly

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == 'Apache License 2.0') {
+  return `
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    `
+  } else if (license == 'GNU GPLv3') {
+  return `
+  [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+      `
+  } else if (license == 'MIT License') {
+  return `
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    `
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +22,37 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license) {
+    return '';
+  } else if (license == 'Apache License 2.0') {
+    return `
+  ### ${license}
+  A permissive license whose main conditions require preservation of copyright and license notices. 
+  Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works 
+  may be distributed under different terms and without source code.
+    `
+  } else if (license == 'GNU GPLv3') {
+  return `
+  ### ${license}
+  Permissions of this strong copyleft license are conditioned on making available complete source code of 
+  licensed works and modifications, which include larger works using a licensed work, under the same license. 
+  Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+  `
+  } else if (license == 'MIT License') {
+  return `
+  ### ${license}
+  A short and simple permissive license with conditions only requiring preservation of copyright and license notices. 
+  Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+  `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
+   return `
   # ${data.title}
+  ${renderLicenseBadge(data.license)}
   
   ## Description
   ${data.description}
@@ -32,14 +71,18 @@ function generateMarkdown(data) {
   ## Credits
   ${data.contributing}
 
-  ## License 
-  ${data.license}
-
   ## Tests
   ${data.tests}
 
   ## Questions?
-  GitHub: [https://github.com/${data.username}](https://github.com/${data.username}) & Email Address: [${data.emailAddress}]{mailto:${data.emailAddress}}
+  GitHub: [https://github.com/${data.username}](https://github.com/${data.username}) & Email Address: <${data.emailAddress}>
+
+  ## License 
+  ${renderLicenseSection(data.license)}
+
+  Copyright (c) [${data.username}](https://github.com/${data.username}). All rights reserved.
+  
+  Licensed under the ${data.license} license.
 `;
 }
 
